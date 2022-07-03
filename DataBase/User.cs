0bf8c6f6b93178637a6f31b9a7e8629785f670e8
Alisson Osvaldo <alisson.artigas@gmail.com>
2022-07-03 01:40:55 -0300

    DataBase users alterado para SQLite

--- DataBase/User.cs
+++ DataBase/User.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.UI.WebControls;
 using System.Windows.Forms;
 
+
 namespace Database
 {
     public class User
     {
         private static  string connectionstring;
+        private const string connectionString = "Data Source= D:\\Save Projects\\Estudo CSharp POO\\SafeBox\\SafeBox\\bin\\Debug\\SQLite\\bd_SafeBox.db";
         //private static string connectionstringStatic;
 
         public User()
@@ -23,20 +26,21 @@ namespace Database
 
         
         public void Gravar(string username, string password)
-        {
+        {        
             try
             {
-                using (SqlConnection connection = new SqlConnection(connectionstring))
+                using (SQLiteConnection connection = new SQLiteConnection(connectionString))
                 {
-                    string queryString = "INSERT INTO registers(username, password) VALUES('" + username + "', '" + password + "')";
-                    SqlCommand command = new SqlCommand(queryString, connection);
+                    string queryString = "INSERT INTO tb_users(username, password) VALUES('" + username + "', '" + password + "')";
+                    SQLiteCommand command = new SQLiteCommand(queryString, connection);
                     command.Connection.Open();
                     command.ExecuteNonQuery();
+                    connection.Close();
 
-                    MessageBox.Show("Cadastro feito com sucesso\nAgora é só entrar utilizando sua senha.");
-                }
+                    MessageBox.Show("Cadastro feito com sucesso\nAgora é só entrar utilizando sua senha.");                  
+                }               
             }
-            catch (System.Data.SqlClient.SqlException)
+            catch (SqlException)
             {
                 MessageBox.Show("Nome de usuário já cadastrado! tente outro. ");
             }
@@ -45,13 +49,15 @@ namespace Database
         //Validação Login
         public bool Login(string username,string password)
         {
-            using (SqlConnection connection = new SqlConnection(connectionstring))
+           
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
             {
-                string queryString = "SELECT * FROM registers WHERE username = '" + username + "' AND password COLLATE Latin1_General_CS_AS = '" + password + "' ";
+                string queryString = "SELECT * FROM tb_users WHERE username = '" + username + "' AND password = '" + password + "' ";
 
-                SqlDataAdapter adapter = new SqlDataAdapter(queryString, connection);
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter(queryString, connection);
                 DataTable table = new DataTable();
                 adapter.Fill(table);
+                connection.Close();
 
                 foreach (DataRow tb in table.Rows)
                 {
@@ -67,18 +73,21 @@ namespace Database
                     return false;
                 }
 
-                //var result = command.ExecuteScalar(); //verifica apenas 1 coluna
             }
+         
+            
         }
+
+        //Testei até aqui obs:senha atual pegou do SQLserver
 
         public static void UpdateUser(int Id, string newUserName, string newPassword)
         {
             try
             {
-                using (SqlConnection connection = new SqlConnection(connectionstring))
-                using (SqlCommand command = connection.CreateCommand())
+                using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+                using (SQLiteCommand command = connection.CreateCommand())
                 {
-                    command.CommandText = "UPDATE registers SET username = @username, password = @newPassword WHERE id = @id ";
+                    command.CommandText = "UPDATE tb_users SET username = @username, password = @newPassword WHERE id = @id ";
 
                     command.Parameters.AddWithValue("@username", newUserName);
                     command.Parameters.AddWithValue("@newPassword", newPassword);
@@ -99,13 +108,14 @@ namespace Database
         //Validar senha atual
         public static bool validationPassword( int id, string password )
         {
-            using (SqlConnection connection = new SqlConnection(connectionstring))
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
             {
-                string queryString = "SELECT * FROM registers WHERE id = '" + id + "' AND password COLLATE Latin1_General_CS_AS = '" + password + "' ";
+                string queryString = "SELECT * FROM tb_users WHERE id = '" + id + "' AND password = '" + password + "' ";
 
-                SqlDataAdapter adapter = new SqlDataAdapter(queryString, connection);
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter(queryString, connection);
                 DataTable table = new DataTable();
-                adapter.Fill(table);               
+                adapter.Fill(table);
+                connection.Close();
 
                 if (table.Rows.Count == 1)
                 {
@@ -127,10 +137,10 @@ namespace Database
 
         public static void DeleteUser(int Id)
         {
-            using (SqlConnection connection = new SqlConnection(connectionstring))
-            using (SqlCommand command = connection.CreateCommand())
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            using (SQLiteCommand command = connection.CreateCommand())
             {
-                command.CommandText = "DELETE FROM registers WHERE id = @id";
+                command.CommandText = "DELETE FROM tb_users WHERE id = @id";
 
                 command.Parameters.AddWithValue("@id", Id);
 
@@ -145,62 +155,24 @@ namespace Database
 
         public DataTable Todos()
         {
-            using (SqlConnection connection = new SqlConnection(connectionstring))
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
             {
-                string queryString = "SELECT * FROM registers";
-                SqlCommand command = new SqlCommand(queryString, connection);
+                string queryString = "SELECT * FROM tb_users";
+                SQLiteCommand command = new SQLiteCommand(queryString, connection);
                 command.Connection.Open();
 
-                SqlDataAdapter adapter = new SqlDataAdapter(); //Esse cara vai pegar nossa requisição(registro) no bd
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter(); //Esse cara vai pegar nossa requisição(registro) no bd
                 adapter.SelectCommand = command;
 
                 DataTable table = new DataTable();
                 adapter.Fill(table);
+                connection.Close();
 
                 return table;
             }
         }
 
-
-
-        /* public static void UpdateUser(int Id, string Password, string newUserName, string newPassword)
-        {          
-          try
-           {
-              using (SqlConnection connection = new SqlConnection(connectionstring))
-              using (SqlCommand command = connection.CreateCommand())
-              {
-                  //command.CommandType = CommandType.StoredProcedure;
-                  //int RowCount = 0;
-
-                  command.CommandText = "UPDATE registers SET username = @username, password = @newPassword WHERE id = @id and password = @password SELECT @@ROWCOUNT"; //'" + RowCount + "'
-
-                  SqlParameter parameter = command.Parameters.Add("@@ROWCOUNT", SqlDbType.Int);
-                  parameter.Direction = ParameterDirection.ReturnValue;
-
-
-                  command.Parameters.AddWithValue("@username", newUserName);
-                  command.Parameters.AddWithValue("@newPassword", newPassword);
-                  command.Parameters.AddWithValue("@password", Password);
-                  command.Parameters.AddWithValue("@id", Id);
-                  //command.Parameters.AddWithValue("@RowCount", RowCount);
-
-                  connection.Open();
-                  command.ExecuteNonQuery();
-
-                  Int32 rowCount = (Int32)command.Parameters["@@ROWCOUNT"].Value;
-                  Console.WriteLine(rowCount);
-                  connection.Close();
-
-
-                  MessageBox.Show("Conta Alterado com sucesso");
-              }
-          }
-          catch (Exception ex)
-          {
-              MessageBox.Show("Error: " + ex.Message);
-          }
-        }*/
+        
 
     }
 }
